ef5c454f7b1dd01f1b64613391fde69c80ce18e3
Vale L <106398144+vlysi@users.noreply.github.com>
2024-07-18 08:51:55 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dice';
+  leftDice = '../assets/img/dice1.png';
+  rightDice = '../assets/img/dice4.png';
+  number1!: number;
+  number2!: number;
+
+  rollTheDice(): void{
+    this.number1 = Math.round(Math.random() * 5) + 1;
+    this.number2 = Math.round(Math.random() * 5) + 1;
+    this.leftDice = `../assets/img/dice${this.number1}.png`;
+    this.rightDice = `../assets/img/dice${this.number2}.png`;
+  }
 }
